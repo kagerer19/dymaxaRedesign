461ae086037d4e47bda090fd590aa2777913cadd
kagerer19 <kagerer19@gmail.com>
2024-01-04 19:19:17 +0100

    Adjust margin top for header logo

--- client/dymaxa-client/src/components/Header.jsx
+++ client/dymaxa-client/src/components/Header.jsx
@@ -8,10 +8,9 @@ const Header = () => {
         <header
             className="px-4 md:px-6 h-16 md:h-20 flex justify-around items-center bg-[#F8F7F2] text-gray-700 border-b-2 border-[#7bf1a8]">
             <div className="flex items-center mb-2 md:mb-0 align-middle">
-                <img className={"header-logo"} src={"src/assets/Dymaxa-logo.png"} alt={"Four leaf clover"}/>
-                <a className="hidden md:flex items-center gap-3 text-lg font-semibold sm:text-base mr-4 mb-1 hover:text-gray-300"
-                   href="#">
-                    <span className="hidden md:inline">Dymaxa Recruiting</span>
+                <img className={"header-logo md:mt-0 mt-3"} src={"src/assets/Dymaxa-logo.png"} alt={"Four leaf clover"}/>
+                <a className="hidden md:flex items-center gap-3 text-lg font-semibold sm:text-base mr-4 mb-1 hover:text-gray-300" href="#">
+                    <span className="hidden md:inline text-black">Dymaxa Recruiting</span>
                 </a>
             </div>
             <div className="flex gap-5">
