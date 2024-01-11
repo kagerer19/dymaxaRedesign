9b1d242b42fb1e90470f3bfa698998ed8bd45d6c
kagerer19 <kagerer19@gmail.com>
2024-01-11 10:25:31 +0100

    Backup in preparation for moving modules to correct directory

--- client/dymaxa-client/src/components/JobsHero.jsx
+++ client/dymaxa-client/src/components/JobsHero.jsx
@@ -1,18 +1,25 @@
 import React from "react";
 import JobsHeader from "../pages/Jobs.jsx";
 import {TextField} from "@mui/material";
+import JobsSearch from "./jobSearch.jsx";
 
 function Jobs() {
     return (
-        <>
-            <div className="jobs-hero-screen">
-                <div className="absolute"></div>
-                <div className="hero-content-jobs">
-                    <h1 className="jobs-hero-text">Jobs</h1>
-                    <TextField label="Search Jobs" className={"search"} Outlined secondary/>
-                </div>
+        <div className="jobs-hero-screen">
+            <div className="absolute"></div>
+            <div
+                className="hero-content-jobs"
+                style={{
+                    maxWidth: '800px', // Adjust the maximum width based on your design
+                    margin: '0 auto', // Center the content
+                    padding: '20px', // Add padding to the content
+                }}
+            >
+                <h1 className="jobs-hero-text">Jobs</h1>
+                <TextField label="Search Jobs" className={"search"} Outlined secondary/>
+                <JobsSearch />
             </div>
-        </>
+        </div>
     )
 }
 
---
+++ client/dymaxa-client/src/components/jobSearch.jsx
@@ -0,0 +1,11 @@
+import {TextField} from "@mui/material";
+
+function JobsSearch() {
+    return (
+        <>
+            <a href="#" className="cta-button" style={{width: '100px', margin: '10px 10px'}}>Search</a>
+        </>
+    )
+}
+
+export default JobsSearch;
