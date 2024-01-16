43cec8d2a101a16a453655f8812b9a3151f1e5e3
kagerer19 <kagerer19@gmail.com>
2024-01-16 15:09:43 +0100

    Adjust routes and styling

--- client/dymaxa-client/src/App.jsx
+++ client/dymaxa-client/src/App.jsx
@@ -2,23 +2,35 @@ import './App.css'
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import NotFound from "./pages/NotFound.jsx";
-import JobDescriptionPage from "./pages/JobDescriptionPage.jsx";
 import JobPage from "./pages/JobPage.jsx";
+import {ToastContainer} from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+import { ProSidebarProvider } from "react-pro-sidebar";
+import LoginPage from "./pages/LoginPage.jsx";
+import UserDashboard from "./pages/user/UserDashboard.jsx";
+import UserRoute from "./components/UserRoute.jsx";
+import Layout from "./pages/global/Layout.jsx";
 
 
+const UserDashboardHOC = Layout(UserDashboard)
 function App() {
     return (
         <>
-            <BrowserRouter>
-                <Routes>
-                    <Route path='/' element={<Home/>} />
-                    <Route path='/JobPage' element={<JobPage/>} />
-                    <Route path='/search/location/:location' element={<JobPage/>} />
-                    <Route path='/search/:keyword' element={<JobPage/>} />
-                    <Route path='/JobDescriptionPage' element={<JobDescriptionPage/>} />
-                    <Route path='*' element={<NotFound/>} />
-                </Routes>
-            </BrowserRouter>
+            <ToastContainer toastStyle={{background: '#F8F7F1'}}/>
+            <ProSidebarProvider>
+                <BrowserRouter>
+                    <Routes>
+                        <Route path='/' element={<Home/>}/>
+                        <Route path='/JobPage' element={<JobPage/>}/>
+                        <Route path='/search/location/:location' element={<JobPage/>}/>
+                        <Route path='/search/:keyword' element={<JobPage/>}/>
+                        <Route path='/LoginPage' element={<LoginPage/>}/>
+                        <Route path='/user/UserDashboard' element={<UserRoute><UserDashboardHOC/></UserRoute>}/>
+                        <Route path='/user/UserJobHistory' element={<UserRoute><UserDashboardHOC/></UserRoute>}/>
+                        <Route path='*' element={<NotFound/>}/>
+                    </Routes>
+                </BrowserRouter>
+            </ProSidebarProvider>
         </>
     )
 }
