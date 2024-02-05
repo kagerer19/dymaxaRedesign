f822aae55b15abe561fb9f7beb0143606a0600c7
kagerer19 <kagerer19@gmail.com>
2024-02-05 16:18:39 +0100

    Prepare for Vercel upload

--- client/dymaxa-client/src/components/DymaxaHeader.jsx
+++ client/dymaxa-client/src/components/DymaxaHeader.jsx
@@ -21,8 +21,8 @@ const DymaxaHeader = () => {
         <header
             className="px-4 md:px-6 h-16 md:h-20 flex justify-around items-center bg-[#F8F7F1]3 border-b-2 border-[#7bf1a8]">
             <div className="flex items-center mb-2 md:mb-0 align-middle">
-                <img className={"header-logo md:mt-0 mt-3"} src={"/assets/Dymaxa-logo copy.png"}
-                     alt={"Four leaf clover"}/>
+                <img src={'/src/assets/comingSoon-img.png'} alt="Delevery bike image"
+                     style={{width: '100%', maxWidth: '600px', marginTop: '6em', marginBottom: '3em'}}/>
                 <a className="hidden md:flex items-center gap-3 text-lg font-semibold sm:text-base mr-4 mb-1 hover:text-gray-300"
                    href="/">
                     <span className="hidden md:inline text-black">Dymaxa Recruiting</span>
