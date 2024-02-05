0ab145f77eac3c7f0cb8addae0696c0c484b0255
kagerer19 <kagerer19@gmail.com>
2024-02-05 16:20:59 +0100

    Prepare for Vercel upload

--- client/dymaxa-client/src/components/DymaxaHeader.jsx
+++ client/dymaxa-client/src/components/DymaxaHeader.jsx
@@ -21,7 +21,7 @@ const DymaxaHeader = () => {
         <header
             className="px-4 md:px-6 h-16 md:h-20 flex justify-around items-center bg-[#F8F7F1]3 border-b-2 border-[#7bf1a8]">
             <div className="flex items-center mb-2 md:mb-0 align-middle">
-                <img src={'/src/assets/comingSoon-img.png'} alt="Delevery bike image"
+                <img src={'dymaxa-client/src/assets/comingSoon-img.png'} alt="Delevery bike image"
                      style={{width: '100%', maxWidth: '600px', marginTop: '6em', marginBottom: '3em'}}/>
                 <a className="hidden md:flex items-center gap-3 text-lg font-semibold sm:text-base mr-4 mb-1 hover:text-gray-300"
                    href="/">
