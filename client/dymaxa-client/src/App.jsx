29786004e8e506cf91eef1a3f0ea8ab3c85ebff8
kagerer19 <kagerer19@gmail.com>
2024-02-05 15:45:55 +0100

    Prepare for Vercel upload

--- client/dymaxa-client/src/App.jsx
+++ client/dymaxa-client/src/App.jsx
@@ -24,7 +24,7 @@ const CurrentJobsHOC = Layout(CurrentJobs)
 const DashCreateJobHOC = Layout(CreateJob)
 const DashUpdateJobHOC = Layout(UpdateJob)
 
-axios.post('https://dymaxa-redesign.vercel.app/')
+axios.post('https://dymaxa-redesign.vercel.app')
 function App() {
     return (
         <>
