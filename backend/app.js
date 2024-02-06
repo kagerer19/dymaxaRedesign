67cc3a78ecc94f7b1a0f5356b11f0df6c618fc97
kagerer19 <kagerer19@gmail.com>
2024-02-06 16:08:10 +0100

    Update vercel.json for CORS

--- backend/app.js
+++ backend/app.js
@@ -1,20 +1,20 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const cors = require("cors");
+const cors = require('cors');
 const mongoose = require("mongoose");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 
-// Import routes
-const authRoutes = require("./routes/authRoutes");
-const userRoutes = require("./routes/userRoutes");
-const jobRoute = require("./routes/jobsRoutes");
+// import routes
+const authRoutes = require('./routes/authRoutes');
+const userRoutes = require('./routes/userRoutes');
+const jobRoute = require('./routes/jobsRoutes');
 
 const cookieParser = require("cookie-parser");
 const errorHandler = require("./middleware/error");
 
-// Database connection
+//database connection
 const dbConnectionString = process.env.DB_CONNECTION_STRING;
 
 mongoose.set("strictQuery", true, "useNewUrlParser", true);
@@ -27,40 +27,57 @@ const connectDB = async () => {
         process.exit(1);
     }
 };
+//MIDDLEWARE
 
-// Middleware
-app.use(morgan("combined"));
+app.use(morgan('combined'));
 app.use(bodyParser.json({ limit: "15mb" }));
-app.use(
-    bodyParser.urlencoded({
-        limit: "15mb",
-        extended: true,
-    })
-);
+app.use(bodyParser.urlencoded({
+    limit: "15mb",
+    extended: true
+}));
 app.use(cookieParser());
+
 app.use(express.json());
 
-console.log("CORS_ALLOWED_ORIGINS:", process.env.CORS_ALLOWED_ORIGINS);
-
 // Apply CORS middleware globally
-app.use(
-    cors({
-        origin: process.env.CORS_ALLOWED_ORIGINS || "https://dymaxa-redesign-frontend.vercel.app", // Allow requests from specified origins
-        methods: ["GET", "POST", "PUT", "DELETE"],
-        credentials: true,
-    })
-);
+app.use(cors({
+    origin: process.env.CORS_ALLOWED_ORIGINS,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}));
 
-// Routes
-app.use("/api", jobRoute);
-app.use("/api", authRoutes);
-app.use("/api", userRoutes);
+app.use(cors(
+    {
+        origin: ["https://dymaxa-redesign-frontend.vercel.app"],
+        methods: ["POST", "GET", "FETCH"],
+        credentials: true
+    }
+));
 
-// Error middleware
+app.use('/api', cors({
+    origin: ["https://dymaxa-redesign-frontend.vercel.app"],
+    methods: ["POST", "GET", "FETCH"],
+    credentials: true
+}), jobRoute);
+
+app.use('/api', cors({
+    origin: ["https://dymaxa-redesign-frontend.vercel.app"],
+    methods: ["POST", "GET", "FETCH"],
+    credentials: true
+}), authRoutes);
+
+app.use('/api', cors({
+    origin: ["https://dymaxa-redesign-frontend.vercel.app"],
+    methods: ["POST", "GET", "FETCH"],
+    credentials: true
+}), userRoutes);
+
+
+// error middleware
 app.use(errorHandler);
 
-// Port
-const port = process.env.PORT || 8000;
+//port
+const port = process.env.PORT || 8000
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
