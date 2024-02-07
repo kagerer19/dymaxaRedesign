7c9663c6b8abc8b18dc320e16af2eeb4e7992ecb
kagerer19 <kagerer19@gmail.com>
2024-02-07 17:59:01 +0100

    Adjust server amd endpoint for Dev testing

--- backend/routes/jobsRoutes.js
+++ backend/routes/jobsRoutes.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const router = express.Router();
+const app = express();
 const {isAdmin, isAuthenticated} = require("../middleware/auth");
 const {createJob, singleJob, updateJob, showJobs, deleteJob} = require("../controllers/jobsController");
+const cors = require("cors");
 
 //Jobs routes
 // /api/job/create
+
+app.use(cors({
+    origin: 'https://dymaxa-redesign-frontend.vercel.app',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+}));
+
+
 router.post('/job/create', createJob);
 
 // /api/job/id
