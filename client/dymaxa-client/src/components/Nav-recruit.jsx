9158863b9b88d51361b2548e4407e474494ea266
kagerer19 <kagerer19@gmail.com>
2024-02-08 13:41:27 +0100

    Adjust flyout menu to display in center of screen on mobile devices

--- client/dymaxa-client/src/components/Nav-recruit.jsx
+++ client/dymaxa-client/src/components/Nav-recruit.jsx
@@ -34,7 +34,7 @@ function NavRecruit() {
                 leaveFrom="opacity-100 translate-y-0"
                 leaveTo="opacity-0 translate-y-1"
             >
-                <Popover.Panel className="absolute z-10 mt-5 flex w-screen max-w-md" style={{ left: 'calc(50% - 230px)' }}>
+                <Popover.Panel className="absolute z-10 mt-5 flex w-screen" style={{ left: 'calc(50% - 230px)', width: "25rem" }}>
                 <div className="w-screen max-w-md flex-auto overflow-hidden rounded-3xl bg-white text-sm leading-6 shadow-lg ring-1 ring-gray-900/5">
                         <div className="p-4">
                             {solutions.map((item) => (
